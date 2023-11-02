5b6a936efc085d57ace8f4f154f77a396c17ed5c
Kyle Krull <kkrull@8thlight.com>
2023-11-02 15:25:03 -0500

    Add optional parameter

--- unittest-greeter/test_greeter.py
+++ unittest-greeter/test_greeter.py
@@ -1,9 +1,13 @@
 import unittest
 
-def greet():
-    return "Hello World"
+def greet(name="World"):
+    return "Hello %s" % (name)
 
 class GreeterGreetTest(unittest.TestCase):
     def test_no_args(self):
         """greets the world, given no arguments"""
-        self.assertEquals(greet(), "Hello World")
+        self.assertEqual(greet(), "Hello World")
+
+    def test_name(self):
+        """greets a person, given a name"""
+        self.assertEqual(greet("George"), "Hello George")
