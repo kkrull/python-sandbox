3f3fcbb3d50b8aa0c7f8a32af99f701ce2db5b25
Kyle Krull <kkrull@8thlight.com>
2023-11-02 15:23:13 -0500

    Add docstring

--- unittest-greeter/test_greeter.py
+++ unittest-greeter/test_greeter.py
@@ -1,5 +1,9 @@
 import unittest
 
+def greet():
+    return "Hello World"
+
 class GreeterGreetTest(unittest.TestCase):
     def test_no_args(self):
+        """greets the world, given no arguments"""
         self.assertEquals(greet(), "Hello World")
