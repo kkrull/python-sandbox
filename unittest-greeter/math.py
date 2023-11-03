a6d32b96a055c8623da31129c290a07103815c03
Kyle Krull <kkrull@8thlight.com>
2023-11-03 07:44:19 -0500

    Add validation check on a

--- unittest-greeter/math.py
+++ unittest-greeter/math.py
@@ -1,3 +1,7 @@
 
 def add(a, b):
+    """adds two numbers, from user input"""
+    if type(a) not in [int]:
+        raise TypeError
+
     return a + b
--- unittest-greeter/test_math.py
+++ unittest-greeter/test_math.py
@@ -6,3 +6,8 @@ class AddTest(unittest.TestCase):
         """should return the sum of two integers"""
         self.assertEqual(3, add(1, 2))
         self.assertEqual(6, add(2, 4))
+
+    def test_add_strings(self):
+        """should raise TypeError for strings that are not numbers"""
+        with self.assertRaises(TypeError):
+            add("1", "2")
