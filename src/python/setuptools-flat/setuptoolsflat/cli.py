d7404ae6f7b93556d9882527bcbc951e32cf1052
Kyle Krull <kdkrull@gmail.com>
2025-03-24 15:02:20 -0500

    Hard-code version

--- src/python/setuptools-flat/setuptoolsflat/cli.py
+++ src/python/setuptools-flat/setuptoolsflat/cli.py
@@ -3,7 +3,12 @@
 from setuptoolsflat.greeting import make_greeting
 
 def main():
+    version = read_version()
+    print(f"Marvin version {version}")
     print(make_greeting())
+
+def read_version():
+    return "Q.38"
 
 if __name__ == '__main__':
     main()
