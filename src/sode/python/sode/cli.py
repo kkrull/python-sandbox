1f63d81c4104d52b8a8845f4ca480f05863a1686
Kyle Krull <kdkrull@gmail.com>
2025-03-26 11:56:10 -0500

    Add --version to sode with environment changes

--- src/sode/python/sode/cli.py
+++ src/sode/python/sode/cli.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 import sys
+from argparse import ArgumentParser
+
+import sode._version
 
 
-def main(argv):
+def main():
+    status = do_main(sys.argv)
+    sys.exit(status)
+
+
+def do_main(argv):
+    parser = ArgumentParser(
+        add_help=True,
+        description="BRODE SODE",
+        prog=argv[0],
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        default=False,
+        help="Print version",
+    )
+
+    args = parser.parse_args(args=argv[1:])
+    if args.version:
+        print(f"Version {sode._version.__version__}")
+        return 0
+
     print("Hello world!")
     return 0
 
 
 if __name__ == "__main__":
-    status = main(sys.argv)
-    sys.exit(status)
+    main()
