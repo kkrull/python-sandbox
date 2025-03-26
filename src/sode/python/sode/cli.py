93c5bdd49cb9441db18c773c9dca6fdbafadc31f
Kyle Krull <kdkrull@gmail.com>
2025-03-26 11:22:04 -0500

    Prepare a main function

--- src/sode/python/sode/cli.py
+++ src/sode/python/sode/cli.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 
-def main():
+
+def main(argv):
     print("Hello world!")
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    status = main(sys.argv)
+    sys.exit(status)
