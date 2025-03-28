479d1e6abeb88a2498ffe82d7d816f526c128b1f
Kyle Krull <kdkrull@gmail.com>
2025-03-28 16:04:23 -0500

    Fix de-structuring

--- src/sode/python/sode/shared/either.py
+++ src/sode/python/sode/shared/either.py
@@ -34,40 +34,44 @@ class EitherBase[A, B]:
 
 
 class Left[A, B](EitherBase[A, B]):
+    __match_args__ = ("value",)
+
     def __init__(self, value: A):
-        self.__value = value
+        self.value = value
 
     def flatMap(self, _fn: Callable[[B], Either[A, C]]) -> Either[A, C]:
-        return Left[A, C](self.__value)
+        return Left[A, C](self.value)
 
     def getOrElse(self, fallbackValue: B) -> B:
         return fallbackValue
 
     def map(self, _fn: Callable[[B], C]) -> Either[A, C]:
-        return Left[A, C](self.__value)
+        return Left[A, C](self.value)
 
     def __repr__(self) -> str:
         return str(self)
 
     def __str__(self) -> str:
-        return f"Left({self.__value})"
+        return f"Left({self.value})"
 
 
 class Right[A, B](EitherBase[A, B]):
+    __match_args__ = ("value",)
+
     def __init__(self, value: B):
-        self.__value = value
+        self.value = value
 
     def flatMap(self, fn: Callable[[B], Either[A, C]]) -> Either[A, C]:
-        return fn(self.__value)
+        return fn(self.value)
 
     def getOrElse(self, _fallbackValue: B) -> B:
-        return self.__value
+        return self.value
 
     def map(self, fn: Callable[[B], C]) -> Either[A, C]:
-        return Right(fn(self.__value))
+        return Right(fn(self.value))
 
     def __repr__(self) -> str:
         return str(self)
 
     def __str__(self) -> str:
-        return f"Right({self.__value})"
+        return f"Right({self.value})"
