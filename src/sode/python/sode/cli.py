2d5a5853220acfa0bc582a7a126107f1bb572f21
Kyle Krull <kdkrull@gmail.com>
2025-03-28 11:09:51 -0500

    Introduce MainState

--- src/sode/python/sode/cli.py
+++ src/sode/python/sode/cli.py
@@ -8,16 +8,33 @@ import sode._version
 
 
 def main() -> NoReturn:
-    status = do_main(sys.argv)
+    state = MainState(
+        argv=sys.argv,
+    )
+
+    status = do_main(state)
     sys.exit(status)
 
 
-def do_main(argv: list[str]) -> int:
+class MainState:
+    _argv: list[str]
+
+    def __init__(self, argv: list[str]):
+        self._argv = argv
+
+    def arguments(self) -> list[str]:
+        return self._argv[1:]
+
+    def program_name(self) -> str:
+        return self._argv[0]
+
+
+def do_main(state: MainState) -> int:
     parser = ArgumentParser(
         add_help=True,
         description="BRODE SODE",
         exit_on_error=False,
-        prog=argv[0],
+        prog=state.program_name(),
     )
 
     parser.add_argument(
@@ -37,7 +54,7 @@ def do_main(argv: list[str]) -> int:
     )
 
     try:
-        args = parser.parse_args(args=argv[1:])
+        args = parser.parse_args(args=state.arguments())
     except ArgumentError as error:
         print(error)
         return 1
