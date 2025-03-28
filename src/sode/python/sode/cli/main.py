ae448892103322e155acdd7df2b60674b41f7ba5
Kyle Krull <kdkrull@gmail.com>
2025-03-28 13:00:59 -0500

    Flatted it out a littled

--- src/sode/python/sode/cli/main.py
+++ src/sode/python/sode/cli/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+import typing
 from argparse import ArgumentError, ArgumentParser
 from typing import NoReturn, Union
 
@@ -21,20 +22,21 @@ def main() -> NoReturn:
 
 def do_main(state: MainState) -> int:
     args: str | SodeNamespace = parse_args(state)
-    match args:
-        case str(error):
-            print(error, file=state.stderr)
-            return 1
-        case SodeNamespace():
-            if args.debug:
-                root_logger = logging.getLogger()
-                root_logger.setLevel(logging.DEBUG)
-            if args.version:
-                print(sode._version.__version__, file=state.stdout)
-                return 0
+    if args is str:
+        print(args, file=state.stderr)
+        return 1
 
-            print("Hello world!", file=state.stdout)
-            return 0
+    # Try making typings for Either: https://stackoverflow.com/a/62282621/112682
+    sodeArgs = typing.cast(SodeNamespace, args)
+    if sodeArgs.debug:
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+    if sodeArgs.version:
+        print(sode._version.__version__, file=state.stdout)
+        return 0
+
+    print("Hello world!", file=state.stdout)
+    return 0
 
 
 def parse_args(state: MainState) -> Union[str, SodeNamespace]:
