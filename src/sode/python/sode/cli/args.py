62d6e4793056f8a2d708504b2438c58d2178c8be
Kyle Krull <kdkrull@gmail.com>
2025-03-28 16:37:33 -0500

    Make it a little more functional

--- src/sode/python/sode/cli/args.py
+++ src/sode/python/sode/cli/args.py
@@ -1,7 +1,24 @@
 from argparse import ArgumentError, ArgumentParser, Namespace
+from dataclasses import dataclass
 
 from sode.cli.state import MainState
 from sode.shared.either import Either, Left, Right
+
+
+@dataclass
+class BoolOption:
+    help: str
+    long_name: str
+    short_name: str
+
+    def add_to(self, parser: ArgumentParser) -> None:
+        parser.add_argument(
+            self.short_name,
+            self.long_name,
+            action="store_true",
+            default=False,
+            help=self.help,
+        )
 
 
 class SodeNamespace(Namespace):
@@ -17,26 +34,25 @@ def parse_args(state: MainState) -> Either[str, SodeNamespace]:
         prog=state.program_name,
     )
 
-    add_global_options(parser)
+    for option in global_options():
+        option.add_to(parser)
+
     try:
         return Right(parser.parse_args(args=state.arguments, namespace=SodeNamespace()))
     except ArgumentError as error:
         return Left(str(error))
 
 
-def add_global_options(parser: ArgumentParser) -> None:
-    parser.add_argument(
-        "-d",
-        "--debug",
-        action="store_true",
-        default=False,
-        help="Log debugging output",
-    )
-
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="store_true",
-        default=False,
-        help="Print version",
-    )
+def global_options() -> list[BoolOption]:
+    return [
+        BoolOption(
+            short_name="-d",
+            long_name="--debug",
+            help="Log debugging output",
+        ),
+        BoolOption(
+            short_name="-v",
+            long_name="--version",
+            help="Print version",
+        ),
+    ]
