098bd9efd91319aec2c27101ada497ebb556e03e
Kyle Krull <kdkrull@gmail.com>
2025-04-02 01:38:51 -0500

    format

--- src/sode/python/sode/shared/cli/namespace.py
+++ src/sode/python/sode/shared/cli/namespace.py
@@ -12,6 +12,17 @@ class ProgramNamespace(Namespace):
     run_selected: CliCommand
 
 
+def add_command_subparsers(
+    main_parser: ArgumentParser,
+) -> _SubParsersAction:  # type: ignore[type-arg]
+    return main_parser.add_subparsers(
+        dest="command",
+        metavar="COMMAND",
+        required=True,
+        title="commands",
+    )
+
+
 def add_global_arguments(parser: ArgumentParser, version: str) -> None:
     """Add global options that are used in this namespace."""
 
@@ -31,14 +42,3 @@ def set_parser_command(parser: ArgumentParser, run_command: CliCommand) -> None:
     """Set the command that will run if this parser is activated."""
 
     parser.set_defaults(run_selected=run_command)
-
-
-def add_command_subparsers(
-    main_parser: ArgumentParser,
-) -> _SubParsersAction:  # type: ignore[type-arg]
-    return main_parser.add_subparsers(
-        dest="command",
-        metavar="COMMAND",
-        required=True,
-        title="commands",
-    )
