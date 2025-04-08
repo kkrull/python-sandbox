fc4a552482fcb4ab8439521c7ba8d6315419c6d1
Kyle Krull <kdkrull@gmail.com>
2025-04-08 18:31:43 -0500

    Read from a token response

--- src/sode/python/sode/soundcloud/config.py
+++ src/sode/python/sode/soundcloud/config.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import asdict, dataclass
+from typing import Any, Mapping
 
 
 @dataclass(frozen=True)
@@ -10,6 +11,10 @@ class TokenResponse:
     refresh_token: str
     scope: list[str]  # ['']
     token_type: str  # Bearer
+
+    @staticmethod
+    def from_oauth2_fetch_token(raw_response: Mapping[str, Any]) -> "TokenResponse":
+        return TokenResponse(**raw_response)
 
     def to_json(
         self,
@@ -25,7 +30,7 @@ class TokenResponse:
         )
 
 
-response = TokenResponse(
+known_response = TokenResponse(
     access_token="abcdef",
     expires_at=1743781923.9585016,
     expires_in=3599,
@@ -34,4 +39,16 @@ response = TokenResponse(
     token_type="Bearer",
 )
 
-print(f"{response.to_json(indent=2, sort_keys=True)}")
+api_response = TokenResponse.from_oauth2_fetch_token(
+    {
+        "access_token": "abcdef",
+        "expires_at": 1743781923.9585016,
+        "expires_in": 3599,
+        "refresh_token": "ABCDEF",
+        "scope": [""],
+        "token_type": "Bearer",
+    }
+)
+
+
+print(f"{api_response.to_json(indent=2, sort_keys=True)}")
