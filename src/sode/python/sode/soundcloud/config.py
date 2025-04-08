e832e9790072a33fd590a50a4d7cad41090d6c64
Kyle Krull <kdkrull@gmail.com>
2025-04-08 18:11:33 -0500

    Add to_json

--- src/sode/python/sode/soundcloud/config.py
+++ src/sode/python/sode/soundcloud/config.py
@@ -5,8 +5,22 @@ from dataclasses import asdict, dataclass
 @dataclass(frozen=True)
 class Response:
     access_token: str
+    expires_in: int
     refresh_token: str
 
+    def to_json(
+        self,
+        indent: int | str | None = None,
+        separators: tuple[str, str] | None = None,
+        sort_keys: bool = False,
+    ) -> str:
+        return json.dumps(
+            asdict(self),
+            indent=indent,
+            separators=separators,
+            sort_keys=sort_keys,
+        )
 
-response = Response(access_token="abcdef", refresh_token="ABCDEF")
-print(json.dumps(asdict(response), indent=2, sort_keys=True))
+
+response = Response(access_token="abcdef", expires_in=3599, refresh_token="ABCDEF")
+print(f"{response.to_json(indent=2, sort_keys=True)}")
