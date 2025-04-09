075897da4bb8bdb585aae5db8a42198b4e0d6011
Kyle Krull <kdkrull@gmail.com>
2025-04-09 09:01:36 -0500

    Add auth arguments

--- src/sode/python/sode/soundcloud/auth/cli.py
+++ src/sode/python/sode/soundcloud/auth/cli.py
@@ -1,9 +1,8 @@
 import logging
+import os
 from argparse import _SubParsersAction
 
-import argcomplete
-
-from sode.shared.cli import ProgramNamespace, RunState, add_command, argfactory
+from sode.shared.cli import ProgramNamespace, RunState, argfactory, cmdfactory
 from sode.soundcloud import SC_COMMAND
 
 logger = logging.getLogger(__name__)
@@ -11,10 +10,11 @@ logger = logging.getLogger(__name__)
 
 def add_auth(
     subcommands: _SubParsersAction,  # type: ignore[type-arg]
+    environ: os._Environ[str] = os.environ,
 ) -> None:
     """Add the auth sub-command"""
 
-    auth_parser = add_command(
+    auth_parser = cmdfactory.add_command(
         subcommands,
         "auth",
         command=_run_auth,
@@ -22,27 +22,38 @@ def add_auth(
         help="authorize with SoundCloud API [start here]",
     )
 
-    auth_parser.add_argument(
-        "--check-token-expiration",
-        action="store_true",
-        help="check if persisted access token has expired",
+    argfactory.completable_argument(
+        argfactory.completion_choices(),
+        auth_parser.add_argument(
+            "--client-id",
+            **argfactory.environ_or_required("SOUNDCLOUD_CLIENT_ID", environ),
+            help="OAuth2 client id with which to request access",
+            nargs=1,
+        ),
     )
 
     argfactory.completable_argument(
         argfactory.completion_choices(),
         auth_parser.add_argument(
-            "--client-id",
-            help="OAuth2 client id with which to request access",
-            nargs="?",
+            "--client-secret",
+            **argfactory.carrier_of_secrets(),
+            **argfactory.environ_or_required("SOUNDCLOUD_CLIENT_SECRET", environ),
+            nargs=1,
         ),
     )
 
     argfactory.completable_argument(
-        argcomplete.completers.ChoicesCompleter(choices=[]),  # type: ignore[no-untyped-call]
+        argfactory.completion_choices(),
         auth_parser.add_argument(
-            "--client-secret",
-            help="OAuth2 client secret with which to request access",
-            nargs="?",
+            "--token-endpoint",
+            **argfactory.environ_or_default(
+                "SOUNDCLOUD_TOKEN_URL",
+                "https://secure.soundcloud.com/oauth/token",
+                environ,
+            ),
+            help="URL to SoundCloud OAuth2 token endpoint (default: %(default)s)",
+            metavar="URL",
+            nargs=1,
         ),
     )
 
