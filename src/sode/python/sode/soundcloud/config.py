15009b27312eea3b9003b2571d49cfa366b73a52
Kyle Krull <kdkrull@gmail.com>
2025-04-08 23:20:45 -0500

    Add properties to tell if expired

--- src/sode/python/sode/soundcloud/config.py
+++ src/sode/python/sode/soundcloud/config.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import asdict, dataclass
+from datetime import datetime, timedelta
 from typing import Any, Mapping, TextIO
 
 
@@ -25,6 +26,27 @@ class TokenResponse:
         """Read from a given TextIO and parse as JSON"""
 
         return TokenResponse.parse(json.load(readable))
+
+    @property
+    def expiration(self) -> datetime:
+        """the datetime at which this access token expires"""
+
+        return datetime.fromtimestamp(self.expires_at)
+
+    @property
+    def expired(self, now: datetime = datetime.now()) -> bool:
+        """true if the access token has expired; false if not"""
+
+        return self.expiration > now
+
+    @property
+    def remaining_time(self, now: datetime = datetime.now()) -> timedelta:
+        """how much time is left before the access token expires, or 0 if it has already expired"""
+
+        if self.expiration <= now:
+            return timedelta()
+        else:
+            return self.expiration - now
 
     def to_json(
         self,
