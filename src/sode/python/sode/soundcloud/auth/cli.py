2410808caeb12e417c54a8913bfe0abd4f6c5835
Kyle Krull <kdkrull@gmail.com>
2025-04-11 18:06:33 -0500

    Extract functions

--- src/sode/python/sode/soundcloud/auth/cli.py
+++ src/sode/python/sode/soundcloud/auth/cli.py
@@ -1,6 +1,8 @@
 import logging
 import os
 from argparse import _SubParsersAction
+from pathlib import Path
+from typing import assert_never
 
 from sode.shared.cli import ProgramNamespace, RunState
 from sode.shared.fp import Either, Left, Right, either
@@ -17,13 +19,13 @@ def add_auth(
 ) -> None:
     """Add the auth sub-command"""
 
-    AuthNamespace.add_command_subparser(subcommands, "auth", _run_auth, environ)
+    AuthNamespace.add_command_subparser(subcommands, "auth", _run_auth_shell, environ)
 
 
-def _run_auth(all_args: ProgramNamespace, state: RunState) -> int:
+def _run_auth_shell(all_args: ProgramNamespace, state: RunState) -> int:
     cmd_args = AuthNamespace.upgrayedd(all_args)
     logger.debug({"soundcloud-auth": cmd_args.to_dict()})
-    match _run_auth_cmd(cmd_args, state):
+    match _run_auth_core(cmd_args, state):
         case Left(error):
             print(error, file=state.stderr)
             return 1
@@ -32,10 +34,7 @@ def _run_auth(all_args: ProgramNamespace, state: RunState) -> int:
             return 0
 
 
-## This module
-
-
-def _run_auth_cmd(args: AuthNamespace, _state: RunState) -> Either[str, int]:
+def _run_auth_core(args: AuthNamespace, _state: RunState) -> Either[str, int]:
     logger.debug(
         {
             "_run_auth_cmd": {
@@ -47,31 +46,38 @@ def _run_auth_cmd(args: AuthNamespace, _state: RunState) -> Either[str, int]:
         },
     )
 
-    match args.state_dir_v.do_try(
-        lambda exception: str(exception),
-        lambda state_dir: state_dir.mkdir(0o700, parents=True, exist_ok=True),
-    ):
-        case Left(state_dir_error):
-            return Left(state_dir_error)
-
-    auth_state_path = args.state_dir_v.map(lambda dir: dir.joinpath("soundcloud-auth.json"))
-    match auth_state_path.do_try(
-        lambda exception: str(exception),
-        lambda file_path: file_path.touch(0o600, exist_ok=True),
-    ):
-        case Left(state_dir_error):
-            return Left(state_dir_error)
-
-    match _fetch_tokens_ns(args):
-        case Left(fetch_error):
-            return Left(fetch_error)
-        case Right(tokens):
-            with open(auth_state_path.right_value, mode="wt") as state_file:
-                tokens.write_json(state_file, indent=2, sort_keys=True)
-                return Right(0)
+    return either.flatten_2_or_left(
+        _ensure_user_file(args.state_dir_v, "soundcloud-auth.json"),
+        _fetch_tokens(args),
+    ).flat_map(lambda x: _run_auth_finally(x))
 
 
-def _fetch_tokens_ns(maybe_args: AuthNamespace) -> Either[str, TokenResponse]:
+def _ensure_user_file(dir: Either[str, Path], filename: str) -> Either[str, Path]:
+    return (
+        dir.do_try(
+            lambda exception: str(exception),
+            lambda state_dir: state_dir.mkdir(0o700, parents=True, exist_ok=True),
+        )
+        .map(lambda dir: dir.joinpath(filename))
+        .do_try(
+            lambda exception: str(exception),
+            lambda file_path: file_path.touch(0o600, exist_ok=True),
+        )
+    )
+
+
+def _fetch_tokens(args: AuthNamespace) -> Either[str, TokenResponse]:
     return either.flatten_3_or_left(
-        maybe_args.token_endpoint_v, maybe_args.client_id_v, maybe_args.client_secret_v
+        args.token_endpoint_v,
+        args.client_id_v,
+        args.client_secret_v,
     ).flat_map(lambda arg: fetch_tokens(arg[0], arg[1], arg[2]))
+
+
+def _run_auth_finally(values: tuple[Path, TokenResponse]) -> Either[str, int]:
+    try:
+        with open(values[0], mode="wt") as state_file:
+            values[1].write_json(state_file, indent=2, sort_keys=True)
+            return Right(0)
+    except Exception as error:
+        return Left(str(error))
