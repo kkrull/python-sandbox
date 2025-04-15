09397fe1563e543a0929ff78b3091fd97fbf4f87
Kyle Krull <kdkrull@gmail.com>
2025-04-14 19:15:24 -0500

    Stub in parsing logic

--- src/sode/python/sode/soundcloud/track/list.py
+++ src/sode/python/sode/soundcloud/track/list.py
@@ -1,10 +1,12 @@
 import typing
 from dataclasses import dataclass
-from typing import Callable, Iterable, assert_never
+from typing import Any, Callable, Iterable, Mapping, assert_never
 
+import requests
 from requests import Response
+from requests_oauthlib import OAuth2Session
 
-from sode.shared.fp.either import Either, Left, Right
+from sode.shared.fp import Either, Left, Right
 from sode.shared.oauth.token import AccessToken
 
 type AccessTokenFactory = Callable[[], Either[str, AccessToken]]
@@ -38,6 +40,10 @@ def list_tracks(state: ListTracksState) -> int:
 
 @dataclass(frozen=True)
 class TrackListing:
+    @staticmethod
+    def parse(response_data: Mapping[str, Any]) -> Either[str, "TrackListing"]:
+        return Left("TrackListing::parse: not implemented")
+
     def track_names(self) -> Iterable[str]:
         # TODO KDK: Work here
         return []
@@ -46,15 +52,29 @@ class TrackListing:
 def _tracks_list(access_token: AccessToken, user_id: str) -> Either[str, TrackListing]:
     """List all tracks published by a user"""
 
-    return Left("list_tracks: not implemented")
+    return (
+        Right[str, OAuth2Session](access_token.oauth_session())
+        .map(lambda session: _tracks_list_raw(session, user_id))
+        .flat_map(lambda response: _response_to_either(response))
+        .map(lambda http_response: http_response.json())
+        .flat_map(lambda json_response: TrackListing.parse(json_response))
+    )
 
 
-def _tracks_list_raw(access_token: AccessToken, user_id: str) -> Response:
+def _tracks_list_raw(session: requests.Session, user_id: str) -> Response:
     """List all tracks published by a user, returning the raw HTTP response"""
 
     # TODO KDK: Look up the actual API endpoint for listing tracks
-    session = access_token.oauth_session()
     return session.get(
         f"https://api.soundcloud.com/users/{user_id}/bogus",
         params={"limit": 1},
     )
+
+
+def _response_to_either(response: Response) -> Either[str, Response]:
+    if response.ok:
+        return Right(response)
+    else:
+        return Left(
+            f"Response indicates failure: status={response.status_code}, text={response.text}"
+        )
